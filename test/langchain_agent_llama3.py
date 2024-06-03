837076d7b56b0a9ba22306c34d506ffccbf4cf1b
hesslee <hess.lee70@gmail.com>
2024-06-03 18:23:25 +0900

    Create langchain_agent_llama3.py

---
+++ test/langchain_agent_llama3.py
@@ -0,0 +1,16 @@
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain_experimental.llms.ollama_functions import OllamaFunctions
+
+# connectstring: altibase+pyodbc://<username>:<password>@<dsnname>?server=<server> & port=<port> & database=<database_name>
+db = SQLDatabase.from_uri("altibase+pyodbc://@PYODBC?encoding=UTF-8")
+
+# llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+llm = OllamaFunctions(model="llama3", format="json")
+
+#agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
+agent_executor = create_sql_agent(llm, db=db, agent_type="tool-calling", verbose=True)
+
+response = agent_executor.invoke({"input": "List the total sales per country. Which country's customers spent the most?"})
+#response = agent_executor.invoke({"input": "Describe the playlisttrack table"})
+print(response)
